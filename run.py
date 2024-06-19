758a8c283ea17433fa6059d4b9aab48ff2d1f63c
Andy Cohen <andycohen32@gmail.com>
2024-06-19 07:40:59 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -28,8 +28,7 @@ def get_sales_data():
         data_str = input("Enter your data here: ")
         
         sales_data = data_str.split(",")
-        validate_data(sales_data)
-
+        
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -56,7 +55,7 @@ def validate_data(values):
     return True
 
 
-def updates_sales_worksheet(data):
+def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided.
     """
@@ -76,7 +75,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+   
+    return surplus_data
  
 
 def main():
@@ -85,8 +90,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    updates_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
